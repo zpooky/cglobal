c9f195d468118dec28c6531f40cabc614abdb39b
Fredrik M Olsson <fredriol@axis.com>
2024-02-27 10:42:52 +0100

    cleanup
    Change-Id: I10d220db97915cb04045b625fa1ea6ada6160bc4

--- queries/c/cglobal.scm
+++ queries/c/cglobal.scm
@@ -1,50 +1,18 @@
 ; int a;
-(declaration type: (primitive_type) declarator: (identifier) @id)
+(declaration type: (_) declarator: (identifier) @id)
 ; int a = ...;
-(declaration type: (primitive_type) declarator: (init_declarator declarator: (identifier) @id value: (_)))
+(declaration type: (_) declarator: (init_declarator declarator: (identifier) @id value: (_)))
 ; int *a;
-(declaration type: (primitive_type) declarator: (pointer_declarator declarator: (identifier) @id))
+(declaration type: (_) declarator: (pointer_declarator declarator: (identifier) @id))
 ; int *a = &b;
-(declaration type: (primitive_type) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_)))
-
-(declaration type: (type_identifier) declarator: (identifier) @id)
-declarator: (array_declarator declarator: (identifier) @id)
-; ↓ this is problematic: int idx = 0;
-; declarator: (init_declarator declarator: (identifier) @id)
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (init_declarator declarator: (identifier) @id value: (_)))
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (init_declarator declarator: (identifier) @id))
-declarator: (parenthesized_declarator (pointer_declarator declarator: (identifier) @id))
-declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_))
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (pointer_declarator declarator: (identifier) @id))
-(declaration type: (type_identifier) declarator: (pointer_declarator declarator: (identifier) @id))
-(declaration (storage_class_specifier) type: (primitive_type) declarator: (pointer_declarator declarator: (identifier) @id))
-(declaration (storage_class_specifier) type: (primitive_type) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration type: (type_identifier) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration (storage_class_specifier) type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (identifier) @id))
-(declaration type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (identifier) @id))
-(declaration (storage_class_specifier) (type_qualifier) type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration (storage_class_specifier) type: (sized_type_specifier type: (primitive_type)) declarator: (identifier) @id)
-(declaration type: (struct_specifier name: (type_identifier)) declarator: (identifier) @id)
-(declaration (storage_class_specifier) type: (primitive_type) declarator: (identifier) @id)
-(declaration (storage_class_specifier) type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration (storage_class_specifier) type: (sized_type_specifier) declarator: (identifier) @id)
-(declaration type: (primitive_type) declarator: (init_declarator declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)) value: (_)))
-(declaration (type_qualifier) type: (primitive_type) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_)))
-(declaration (storage_class_specifier) type: (primitive_type) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
+(declaration type: (_) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_)))
 ; enum type { VALUE0 } name;
 (declaration type: (enum_specifier name: (type_identifier) body: (enumerator_list (enumerator name: (identifier)))) declarator: (identifier) @id)
-; static uint8_t name = STATE;
-(declaration (storage_class_specifier) type: (primitive_type) declarator: (init_declarator declarator: (identifier) @id value: (_)))
-; static const int name = CALL(ARG);
-(declaration (storage_class_specifier) (type_qualifier) type: (primitive_type) declarator: (init_declarator declarator: (identifier) @id value: (_)))
-; static const type name = CALL(ARG);
-(declaration (storage_class_specifier) (type_qualifier) type: (type_identifier) declarator: (init_declarator declarator: (identifier) @id value: (_)))
-; static char **name = NULL;
-(declaration (storage_class_specifier) type: (type_identifier) declarator: (init_declarator declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)) value: (null)))
+; static uint8_t var = STATE;
 ; static struct Struct var = { };
-(declaration (storage_class_specifier) type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (identifier) @id value: (_)))
+(declaration (storage_class_specifier) type: (_) declarator: (init_declarator declarator: (identifier) @id value: (_)))
+; static char **var = NULL;
+(declaration (storage_class_specifier) type: (_) declarator: (init_declarator declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)) value: (null)))
 ; const struct Struct var = { };
 (declaration (type_qualifier) type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (identifier) @id value: (_)))
 ; typedef int/struct Struct (*var)(...);
@@ -53,3 +21,51 @@ declarator: (init_declarator declarator: (pointer_declarator declarator: (identi
 (declaration type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (identifier) @id value: (_)))
 ; struct Struct *f2 = ...;
 (declaration type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_)))
+; char var[] = ...;
+(declaration type: (_) declarator: (init_declarator declarator: (array_declarator declarator: (identifier) @id) value: (_)))
+; char var[];
+(declaration type: (_) declarator: (array_declarator declarator: (identifier) @id))
+; static struct Struct var;
+(declaration (storage_class_specifier) type: (_) declarator: (identifier) @id)
+; static struct Struct *var;
+(declaration (storage_class_specifier) type: (_) declarator: (pointer_declarator declarator: (identifier) @id))
+; static struct Struct **var;
+(declaration (storage_class_specifier) type: (_) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
+; const struct Struct var;
+(declaration (type_qualifier) type: (struct_specifier name: (type_identifier)) declarator: (identifier) @id)
+; const struct Struct *var;
+(declaration (type_qualifier) type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (identifier) @id))
+; const struct Struct **var;
+(declaration (type_qualifier) type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
+; struct Struct var;
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (identifier) @id)
+; struct Struct *var;
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (identifier) @id))
+; struct Struct **var;
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (identifier) @id)))
+; Struct_t var;
+(declaration type: (type_identifier) declarator: (identifier) @id)
+; Struct_t var = ...;
+(declaration type: (type_identifier) declarator: (init_declarator declarator: (identifier) @id value: (initializer_list (initializer_pair designator: (field_designator (field_identifier)) value: (_)))))
+; Struct_t *var;
+(declaration type: (type_identifier) declarator: (pointer_declarator declarator: (identifier) @id))
+; Struct_t *var = ...;
+(declaration type: (type_identifier) declarator: (init_declarator declarator: (pointer_declarator declarator: (identifier) @id) value: (_)))
+; int (*var)(...);
+(declaration type: (_) declarator: (function_declarator declarator: (parenthesized_declarator (pointer_declarator declarator: (identifier) @id)) parameters: (_)))
+; int* (*var)(...);
+(declaration type: (_) declarator: (pointer_declarator declarator: (function_declarator declarator: (parenthesized_declarator (pointer_declarator declarator: (identifier) @id)) parameters: (_))))
+; struct Struct var[...];
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (array_declarator declarator: (identifier) @id size: (_)))
+; struct Struct var[...] = {...};
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (array_declarator declarator: (identifier) @id size: (_)) value: (_)))
+; struct Struct *var[...];
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (array_declarator declarator: (identifier) @id size: (_))))
+; struct Struct **var[...];
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (pointer_declarator declarator: (pointer_declarator declarator: (array_declarator declarator: (identifier) @id size: (_)))))
+; struct Struct var[...] = {...};
+(declaration type: (struct_specifier name: (type_identifier)) declarator: (init_declarator declarator: (pointer_declarator declarator: (array_declarator declarator: (identifier) @id size: (_))) value: (_)))
+; char *var[....] = {...};
+(declaration type: (_) declarator: (init_declarator declarator: (pointer_declarator declarator: (array_declarator declarator: (identifier) @id size: (_))) value: (_)))
+; int *var[_];
+(declaration type: (_) declarator: (pointer_declarator declarator: (array_declarator declarator: (identifier) @id size: (_))))
